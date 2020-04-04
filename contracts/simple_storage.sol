46078aea499ca1620a5d4fddff39ee99f7d6cb35
dhruvinparikh93 <imsilent93@gmail.com>
2020-04-04 10:08:38 -0400

    add embark

---
+++ contracts/simple_storage.sol
@@ -0,0 +1,19 @@
+pragma solidity ^0.6.1;
+
+contract SimpleStorage {
+  uint public storedData;
+
+  event Set(address caller, uint _value);
+
+  constructor(uint initialValue) public {
+    storedData = initialValue;
+  }
+
+  function set(uint x) public { storedData = x;
+    emit Set(msg.sender, x);
+  }
+
+  function get() public view returns (uint retVal) {
+    return storedData;
+  }
+}
